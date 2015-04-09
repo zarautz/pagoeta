7f212a9bacfce6612c6ec435174bf9c3eddd4652
Eneko Illarramendi <eneko.illarramendi@elis.ugent.be>
2015-04-09 14:44:42 +0200

    Change camelCasing strategy for `target_age` and `target_group`

--- pagoeta/apps/events/serializers.py
+++ pagoeta/apps/events/serializers.py
@@ -17,12 +17,10 @@ class TypeField(serializers.RelatedField):
 
 class EventSerializer(TranslatableModelSerializer):
     category = TypeField(read_only=True)
-    target_group = TypeField(read_only=True)
-    targetGroup = target_group
-    target_age = TypeField(read_only=True)
-    targetAge = target_age
     place = PlaceListSerializer(read_only=True)
     # camelCase some field names
+    targetGroup = TypeField(source='target_group', read_only=True)
+    targetAge = TypeField(source='target_age', read_only=True)
     startAt = serializers.DateTimeField(source='start_at', read_only=True)
     endAt = serializers.DateTimeField(source='end_at', read_only=True)
     isFeatured = serializers.BooleanField(source='is_featured', read_only=True)
@@ -31,7 +29,7 @@ class EventSerializer(TranslatableModelSerializer):
 
     class Meta(object):
         model = Event
-        exclude = ('start_at', 'end_at', 'is_featured', 'is_visible', 'language_code')
+        exclude = ('target_group', 'target_age', 'start_at', 'end_at', 'is_featured', 'is_visible', 'language_code')
 
     def get_href(self, obj):
         return get_absolute_uri(reverse('v1:event-detail', [obj.id]))
