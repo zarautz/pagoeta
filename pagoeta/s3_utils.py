4f78fe4fd3697a5eb47a710cda3d662352f948f3
Eneko Illarramendi <eneko@illarra.com>
2016-10-09 19:55:08 +0200

    Fix bug with S3 storage

--- pagoeta/s3_utils.py
+++ pagoeta/s3_utils.py
@@ -1,7 +1,5 @@
 from storages.backends.s3boto import S3BotoStorage
 
 
-def MediaS3BotoStorage(): S3BotoStorage(location='media')
-
-
-def StaticS3BotoStorage(): S3BotoStorage(location='static')
+MediaS3BotoStorage = lambda: S3BotoStorage(location='media')  # noqa
+StaticS3BotoStorage = lambda: S3BotoStorage(location='static')  # noqa
